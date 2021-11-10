0f4f5e994c4b74e79916c149e0020a2c8d17f4c5
floki <flokitheprogrammer@gmail.com>
2021-11-11 01:30:14 +0500

    finalized and finished the progress.

--- script.js
+++ script.js
@@ -6,17 +6,38 @@ let rangeSelected;
 
 // To display the range text on the interface of the application
 const rangeText = document.getElementById("range-text");
+let penColor = "black";
 
 // To set the default value of the range on page laod
 window.onload = changeValue();
 
+//buttons and event listeners to change the pen color and clear start here
+let blackButton = document.getElementById("black-button");
+blackButton.addEventListener("click", () => (penColor = "black"));
+
+let clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", clearEverything);
+
+function clearEverything() {
+  //function for clearing everything triggered with clearButton event listener
+  let listToClear = document.querySelectorAll(".box");
+  for (i = 0; i < listToClear.length; i++) {
+    listToClear[i].setAttribute("style", "background-color: none;");
+  }
+}
+
+let rgbButton = document.getElementById("color-picker");
+rgbButton.addEventListener("change", () => (penColor = rgbButton.value));
+//buttons and event listeners to change the pen color and clear end here
 
 function changeValue() {
+  //main function triggers everything when value of the toggle is changed
   rangeSelected = document.getElementById("grid").value;
   rangeText.innerText = "Range: " + rangeSelected + " x " + rangeSelected;
   removeElements();
   changeGridVals();
   createDivs();
+  changeBg();
 }
 
 function removeElements() {
@@ -26,10 +47,10 @@ function removeElements() {
   }
 }
 
-function changeGridVals () {
-    let draw = document.getElementById("drawing-area");
-    draw.style["grid-template-columns"] = "repeat(" + rangeSelected + ", 1fr)";
-    draw.style["grid-template-rows"] = "repeat(" + rangeSelected + ", 1fr)";
+function changeGridVals() {
+  let draw = document.getElementById("drawing-area");
+  draw.style["grid-template-columns"] = "repeat(" + rangeSelected + ", 1fr)";
+  draw.style["grid-template-rows"] = "repeat(" + rangeSelected + ", 1fr)";
 }
 
 function createDivs() {
@@ -41,5 +62,10 @@ function createDivs() {
   }
 }
 
-let box = document.getElementsByClassName("box");
-
+function changeBg() {
+  document.querySelectorAll(".box").forEach((item) => {
+    item.addEventListener("mouseenter", (event) => {
+      item.style["background-color"] = penColor;
+    });
+  });
+}
